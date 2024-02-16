23c26b924a9197cefa908b8293ee10c2f04eb241
Christian <christian.santos92@gmail.com>
2024-02-16 19:32:15 -0300

    Ajuste de host

--- app.py
+++ app.py
@@ -192,4 +192,4 @@ def search():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='192.168.0.108', port='5000', debug=True)
+    app.run(host='localhost', port='5000', debug=True)
